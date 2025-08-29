1f66eb6a031f6abde468c60f5b46cf002fca1c20
newcodesubed <subedshahxi@gmail.com>
2025-08-29 11:51:27 +0545

    feat: database setup

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -7,6 +12,7 @@ app.get("/",(req,res)=>{
 })
 
 app.listen(3000,()=>{
+    connectDB();
     console.log("server running on port 3000")
 })
 
