ee51bb75d7727c6c259f8b976f93cc7634847c7d
newcodesubed <subedshahxi@gmail.com>
2025-08-29 12:09:58 +0545

    feat: added route setup

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const app = express();
 app.get("/",(req,res)=>{
     res.send("Hello World 1234");
 })
+
+app.use("/api/auth", authRoutes)
 
 app.listen(3000,()=>{
     connectDB();
