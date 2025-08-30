20aa18d66989a6f0f2dc638af23efe1ccbbc2936
newcodesubed <subedshahxi@gmail.com>
2025-08-30 22:49:14 +0545

    feat: implementing login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,17 +3,17 @@ import { motion } from "framer-motion";
 import { Mail, Lock, Loader } from "lucide-react";
 import { Link } from "react-router-dom";
 import Input from "../components/Input";
-// import { useAuthStore } from "../store/authStore";
+import { useAuthStore } from "../store/authStore";
 
 function  LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const isLoading = false
-  // const {isLoading, login, error}=useAuthStore();
+  
+  const {isLoading, login, error}=useAuthStore();
 
   const handleLogin = async (e) => {
 		e.preventDefault();
-		
+		await login(email, password)
 	};
 
 
@@ -52,7 +52,7 @@ function  LoginPage() {
 							Forgot password?
 						</Link>
 					</div> 
-					{/* {error && <p className="text-red-500 font-semibold text-sm mb-2">{error}</p>} */}
+					{error && <p className="text-red-500 font-semibold text-sm mb-2">{error}</p>}
           			<motion.button
 						whileHover={{ scale: 1.02 }}
 						whileTap={{ scale: 0.98 }}
